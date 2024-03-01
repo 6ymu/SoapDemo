6cdae4406f5e67888a6f983f567b33790db3b785
Mukhan <mukhan.zamanbekov@airastana.com>
2024-03-01 13:59:06 +0500

    [r] and ConnectionStrings

--- SoapDemo/SoapDemo.asmx.cs
+++ SoapDemo/SoapDemo.asmx.cs
@@ -1,74 +1,56 @@
-﻿using SoapDemo.Models;
-using System;
-
-
-using System.Web;
-using System.Web.Services;
-
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Data;
-using System.Threading.Tasks;
+using System.Web.Services;
 using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Linq;
+using SoapDemo.Models;
 
 namespace SoapDemo
 {
-    /// <summary>
-    /// Summary description for SoapDemo
-    /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
-    public class SoapDemo : System.Web.Services.WebService
-    {
-      
+	/// <summary>
+	/// Summary description for SoapDemo
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	public class SoapDemo : WebService
+	{
+		[WebMethod]
+		public string HelloWorld()
+		{
+			return "Hello World";
+		}
 
-        [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hello World";
-        }
+		[WebMethod]
+		public ResponseModel<string> login(string email, string password)
+		{
+			ResponseModel<string> response = new ResponseModel<string>();
 
+			if (email != null)
+			{
+				using (SqlConnection conn = new SqlConnection(@"Data Source=ALA-IT-48131\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+				{
+					SqlCommand cmd = new SqlCommand("sp_loginUser", conn);
+					cmd.CommandType = CommandType.StoredProcedure;
+					cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50).Value = email;
+					cmd.Parameters.Add("@password", SqlDbType.NVarChar, 50).Value = password;
 
-        [WebMethod]
-        public  ResponseModel<string> login(string email, string password)
-        {
-            ResponseModel<string> response = new ResponseModel<string>();
+					SqlDataAdapter da = new SqlDataAdapter(cmd);
+					DataTable dt = new DataTable();
+					da.Fill(dt);
 
-            if (email != null)
-            {
-                using (SqlConnection conn = new SqlConnection(@"Server=CODERSIGN\SQLEXPRESS01;Database=IdentityDemo;User Id=freecode;Password=freespot;"))
-                {
-                    SqlCommand cmd = new SqlCommand("sp_loginUser",conn);
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50).Value = email;
-                    cmd.Parameters.Add("@password", SqlDbType.NVarChar, 50).Value = password;
-                    
-
-                    SqlDataAdapter da = new SqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-
-
-                    if (dt.Rows.Count > 0)
-                    {
-                        response.Data = JsonConvert.SerializeObject(dt);
-                        response.resultCode = 200;
-                    }
-                    else
-                    {
-                        response.message = "User Not Found!";
-                        response.resultCode = 500;
-                    }
-
-
-                }
-            }
-            return response;
-        }
-
-    }
+					if (dt.Rows.Count > 0)
+					{
+						response.Data = JsonConvert.SerializeObject(dt);
+						response.resultCode = 200;
+					}
+					else
+					{
+						response.message = "User Not Found!";
+						response.resultCode = 500;
+					}
+				}
+			}
+			return response;
+		}
+	}
 }
